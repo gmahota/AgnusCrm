54b2aaf460b19d79db947f60f098f531b48fe663
guimaraes mahota <guimaraesmahota@outlook.com>
2018-11-25 19:03:32 +0200

    Price List Managment

---
+++ AgunsCrm.Web/AgunsCrm.Web/Scripts/Import_Dados_Defeito.sql
@@ -0,0 +1,11 @@
+﻿--insert into AgnusCrm.dbo.brand 
+--select Marca, Descricao from primarin.dbo.marcas
+ 
+--insert into family
+ --select familia, Descricao from primarin.dbo.familias
+ 
+--insert into subfamily
+ --select subfamilia, Descricao, familia from primarin.dbo.subfamilias
+ use primarin
+ select a.artigo, a.descricao, am.pvp1 from primarin.dbo.Artigo a
+ inner join artigomoeda am on am.artigo = a.artigo and am.moeda like 'M%'
